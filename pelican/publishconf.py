0ccb95a512bc4c87a0588179bbadaaa6bab5480b
40323255 <40323255@gm.nfu.edu.tw>
2016-01-07 00:46:35 +0800

    40323255

--- pelican/publishconf.py
+++ pelican/publishconf.py
@@ -29,6 +29,7 @@ LINKS = (('2015課程網頁', 'http://wordpress-2015course.rhcloud.com/'),
          ('班級個人網頁','http://2015fallhw.github.io/2015fallcadpb/user/40323255/'),
          ('班級組別倉儲', 'http://2015fallhw.github.io/2015fallcadpb/category/g2.html'),
          ('班級github', 'https://github.com/2015fallhw/2015fallcadpb/wiki'),
+         ('個人github倉儲', 'https://github.com/40323255/40323255cadp/tree/gh-pages'),
          ('Python', 'http://python.org/'),
          ('You can modify those links in your config file', '#'),)
 
